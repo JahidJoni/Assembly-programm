c06a270edda438d21bf1faa77e6f6752e771230c
Jahid Joni <jh.joni07@gmail.com>
2021-11-18 21:54:52 +0600

    first commit

---
+++ Printing characters.asm
@@ -0,0 +1,142 @@
+;main procedure starts
+main proc   
+   ;stores the data 
+   MOV DL, 50D
+   ;stores 2 on high accumulator
+   MOV AH, 02H 
+   ;console interupt called
+   INT 21H  
+   
+   MOV DL, 48D
+   MOV AH, 02H
+   INT 21H 
+   
+   MOV DL, 49D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 56D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 49D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 48D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 48D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 48D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 49D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 48D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 48D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 48D
+   MOV AH, 02H
+   INT 21H
+   
+   MOV DL, 54D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 45D
+   MOV AH, 02H
+   INT 21H  
+   
+   
+   MOV DL, 77D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 68D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 32D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 74D
+   MOV AH, 02H
+   INT 21H  
+   
+   MOV DL, 97D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 104D
+   MOV AH, 02H
+   INT 21H    
+   
+   
+   MOV DL, 105D
+   MOV AH, 02H
+   INT 21H    
+   
+   MOV DL, 100D
+   MOV AH, 02H
+   INT 21H   
+   
+   
+   MOV DL, 32D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 72D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 97D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 115D
+   MOV AH, 02H
+   INT 21H    
+   
+   MOV DL, 97D
+   MOV AH, 02H
+   INT 21H
+   
+   MOV DL, 110D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 32D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 74D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 111D
+   MOV AH, 02H
+   INT 21H    
+   
+   MOV DL, 110D
+   MOV AH, 02H
+   INT 21H   
+   
+   MOV DL, 105D
+   MOV AH, 02H
+   INT 21H  
+    
+end   
+;produre ends 
